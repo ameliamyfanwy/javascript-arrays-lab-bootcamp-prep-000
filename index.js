9d79982314e9283cd30ab8023e4c8bbd5016dcc6
amelialloyd31 <amelialloyd31@gmail.com>
2019-02-04 21:57:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions and code here
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.splice(-1)
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  kittens.concat(name)
+}
